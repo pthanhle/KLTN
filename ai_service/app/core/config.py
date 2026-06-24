d1a5d8034595dc8ba2dac64991e73ef151641712
Tien Truong <truongthanhtien2004@gmail.com>
2026-06-24 13:50:22 +0700

    fix : fix AI config

--- ai_service/app/core/config.py
+++ ai_service/app/core/config.py
@@ -7,7 +7,7 @@ class Settings:
     MONGO_URI = os.getenv("MONGO_URI")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     DATABASE_NAME = "carShop"
-    MODEL_NAME = "gemini-2.0-flash"
+    MODEL_NAME = "gemini-2.5-flash"
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 settings = Settings()
